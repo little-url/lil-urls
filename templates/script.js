17e03e3152247208e844291bb047423ff43c0ba1
Sushil Mohan <sushilmohan@Sushils-MacBook-Air.local>
2024-12-18 18:31:56 +0530

    removed script file

--- templates/script.js
+++
@@ -1,7 +0,0 @@
-function copyToClipboard(text) {
-    navigator.clipboard.writeText(text).then(() => {
-        alert('Shortened URL copied to clipboard!');
-    }).catch(err => {
-        console.error('Failed to copy text: ', err);
-    });
-}
